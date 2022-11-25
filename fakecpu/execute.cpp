518e2fb5960cb928bec9a37ca72c133d5b74dbc6
battlecatsahead <48861210+battlecatsahead@users.noreply.github.com>
2022-11-24 19:44:31 -0500

    Add files via upload

--- fakecpu/execute.cpp
+++ fakecpu/execute.cpp
@@ -5,25 +5,54 @@
 using namespace std;
 
 string opmode = "read";
+int addnum1;
+int addnum2;
+int addstep = 1;
+int result;
 
 void execute(string command){
     for(int i = 0; i < command.length(); i++){
-        if(command =="prt"){
-            opmode = "print";
+        if(opmode != "add"){
+            if(command == "prt"){        // print opcode
+                opmode = "print";
+                command = "";
+            }
+
+            
+        }
+        if(command == "add"){        // add opcode
+            opmode = "add";
             command = "";
-        }
-        else{
-            cout << "error: uknown opcode\n";
-        }
+        }       
 
     }
-    if(opmode == "print"){
+    if(opmode == "print"){          //print opcode function
         if(command.back() == '\\'){
             command.pop_back();
             cout << command << "\n";
         }
         else if(command.back() != '\\'){
             cout << command;
+        }
+    }
+    
+    if(opmode == "add"){
+        cout << command;
+        if(addstep == 1){
+            //addnum1 = stoi(command);
+            cout << command;
+            addstep = 2;
+        }
+        if(addstep == 2){
+            //addnum2 = stoi(command);
+            cout << command;
+            addstep = 3;
+        }
+        if(addstep == 3){
+            result = addnum1 + addnum2;
+            cout << command;
+            //cout << result;
+            opmode = "read";
         }
     }
 }
--- fakecpu/main.cpp
+++ fakecpu/main.cpp
@@ -1,3 +1,27 @@
+/*
+list of implemented opcodes:
+prt /print text
+
+wip opcodes:
+add /add 2 numbers
+
+unimplemented opcodes:
+
+sub /substract 2 numbers
+div /divide 2 numbers
+int /create a variable 
+asi /assign a value to a variable
+pri /print a variable
+del /clear a variable
+if  /self explanatory
+inc /add 1 to a variable
+dec //substrac 1 to a variable
+_____________________________________________________________
+list of features to implement:
+memory 
+*/
+
+
 #include <iostream>
 #include <stdio.h>
 #include <string>
@@ -5,7 +29,7 @@
 
 using namespace std;
 
-string hello = "prt,hello\\,";
+string hello = "add,2,3";
 string curop;
 string opcode;
 
@@ -14,6 +38,7 @@ int main(){
         
         if(hello[i] != ','){
             curop += hello[i];
+            
         }
         if(hello[i] == ','){
             execute(curop);
